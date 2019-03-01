2de5fdba995f6338f411120cf03a268be8baa15f
bkwillia052 <bkwillia052@gmail.com>
2019-03-01 12:17:32 -0600

    search challenge and Answers.md completed; MVP complete

--- Data_Structures_Answers.md
+++ Data_Structures_Answers.md
@@ -1,18 +1,23 @@
 Add your answers to the questions below.
 
 1. What is the runtime complexity of your `depth_first_for_each` method?
+   O(log n)
 
 2. What is the space complexity of your `depth_first_for_each` function?
+   O(log n)
 
 3. What is the runtime complexity of your `breadth_first_for_each` method?
-
+   n/a
 4. What is the space complexity of your `breadth_first_for_each` method?
+   n/a
 
+5) What is the runtime complexity of the provided code in `names.py`?
+   O(n\*\*2)
+6) What is the space complexity of the provided code in `names.py`?
+   O(n)
 
-5. What is the runtime complexity of the provided code in `names.py`?
+7) What is the runtime complexity of your optimized code in `names.py`?
+   O(n log n)
 
-6. What is the space complexity of the provided code in `names.py`?
-
-7. What is the runtime complexity of your optimized code in `names.py`?
-
-8. What is the space complexity of your optimized code in `names.py`?
+8) What is the space complexity of your optimized code in `names.py`?
+   O(n)
--- search/binary_search_tree.py
+++ search/binary_search_tree.py
@@ -1,46 +1,63 @@
 class BinarySearchTree:
-  def __init__(self, value):
-    self.value = value
-    self.left = None
-    self.right = None
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
 
-  def depth_first_for_each(self, cb):
-    pass    
+    def depth_first_for_each(self, cb):
+        cb(self.value)
 
-  def breadth_first_for_each(self, cb):
-    pass
+        if self.left:
+            self.left.depth_first_for_each(cb)
+        if self.right:
+            self.right.depth_first_for_each(cb)
 
-  def insert(self, value):
-    new_tree = BinarySearchTree(value)
-    if (value < self.value):
-      if not self.left:
-        self.left = new_tree
-      else:
-        self.left.insert(value)
-    elif value >= self.value:
-      if not self.right:
-        self.right = new_tree
-      else:
-        self.right.insert(value)
+    def breadth_first_for_each(self, cb):
+        pass
 
-  def contains(self, target):
-    if self.value == target:
-      return True
-    if self.left:
-      if self.left.contains(target):
-        return True
-    if self.right:
-      if self.right.contains(target):
-        return True
-    return False
+    def insert(self, value):
+        new_tree = BinarySearchTree(value)
+        if (value < self.value):
+            if not self.left:
+                self.left = new_tree
+            else:
+                self.left.insert(value)
+        elif value >= self.value:
+            if not self.right:
+                self.right = new_tree
+            else:
+                self.right.insert(value)
 
-  def get_max(self):
-    if not self:
-      return None
-    max_value = self.value
-    current = self
-    while current:
-      if current.value > max_value:
-        max_value = current.value
-      current = current.right
-    return max_value
+    def contains(self, target):
+        if self.value == target:
+            return True
+        if self.left:
+            if self.left.contains(target):
+                return True
+        if self.right:
+            if self.right.contains(target):
+                return True
+        return False
+
+    def get_max(self):
+        if not self:
+            return None
+        max_value = self.value
+        current = self
+        while current:
+            if current.value > max_value:
+                max_value = current.value
+            current = current.right
+        return max_value
+
+
+b_tree = BinarySearchTree(5)
+arr = []
+b_tree.insert(2)
+b_tree.insert(3)
+b_tree.insert(7)
+b_tree.insert(9)
+
+b_tree.depth_first_for_each(lambda x: arr.append(x))
+
+print(arr)
